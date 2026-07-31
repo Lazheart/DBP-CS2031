a663e9a8dce9f5472c00f8918d2118ff30ad6425
lazheart <176245760+Lazheart@users.noreply.github.com>
2026-07-31 08:33:20 -0500

    feat: worflow and componetws

---
+++ src/pages/CommingSoonPage.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const CommingSoonPage = () => {
+    return (
+        <div className="commin g-soon w-full flex flex-col h-screen justify-center items-center gap-10">
+            <span className="text-6xl font-bold animate-pulse">Proximamente</span>
+            <p className="text-2xl font-semibold animate-pulse">Nos encontramos trabajando para completar esta seccion vuelve pronto!.</p>
+            <Link to="/" className="text-xl font-semibold animate-pulse">Back to Home</Link>
+        </div>
+    )
+}
+
+export default CommingSoonPage;
