099b858b382b025707d30d18324efc8bf2508e7b
lazheart <176245760+Lazheart@users.noreply.github.com>
2026-08-01 00:30:02 -0500

    feat: implement react-router routing with Home, Coming Soon, and 404 pages

--- src/pages/CommingSoonPage.tsx
+++ src/pages/CommingSoonPage.tsx
@@ -5,9 +5,8 @@ const CommingSoonPage = () => {
         <div className="commin g-soon w-full flex flex-col h-screen justify-center items-center gap-10">
             <span className="text-6xl font-bold animate-pulse">Proximamente</span>
             <p className="text-2xl font-semibold animate-pulse">Nos encontramos trabajando para completar esta seccion vuelve pronto!.</p>
-            <Link to="/" className="text-xl font-semibold animate-pulse">Back to Home</Link>
+            <Link to="/" className="text-xl font-semibold animate-pulse">Ir al inicio</Link>
         </div>
     )
 }
-
 export default CommingSoonPage;
---
+++ src/router/index.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom"
+import NotFoundPage from "../pages/NotFoundPage"
+import HomePage from "../pages/HomePage"
+import CommingSoonPage from "../pages/CommingSoonPage"
+
+const router = createBrowserRouter([
+  // Mapeador de Rutas
+   {
+     path: "/",
+     element: <HomePage />,
+   },
+    {
+     path: "/homepage",
+     element: <HomePage />,
+   },
+    {
+     path: "/comming-soon",
+     element: <CommingSoonPage />,
+   },
+   {
+     path: "/not-found",
+     element: <NotFoundPage />,
+   },
+
+  {
+    // Cualquier ruta no mapeada
+    path: "*",
+    element: <NotFoundPage />,
+  },
+])
+
+export default router
