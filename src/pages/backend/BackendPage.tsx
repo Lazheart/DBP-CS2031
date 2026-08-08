6d4ba906a1080f2d90611adfd5ef15c45ec441c7
lazheart <176245760+Lazheart@users.noreply.github.com>
2026-08-07 21:33:24 -0500

    feat: add initial page skeletons for workflows, infrastructure, and tools modules

---
+++ src/pages/backend/BackendPage.tsx
@@ -0,0 +1,10 @@
+const BackendPage = () => {
+    return (
+        <div className="backend-page">
+            <h1>Backend</h1>
+            <p>En construccion</p>
+        </div>
+    )
+}
+
+export default BackendPage ;
---
+++ src/pages/tools/GithubPage.tsx
@@ -0,0 +1,10 @@
+const GithubPage = () => {
+    return (
+        <div className="github-page">
+            <h1>Github</h1>
+            <p>En construccion</p>
+        </div>
+    )
+}
+
+export default GithubPage;
