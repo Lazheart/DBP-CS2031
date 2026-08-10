baa8b0bf0ec916a9c4203a7b2b295bef00b7b2bd
lazheart <176245760+Lazheart@users.noreply.github.com>
2026-08-10 13:57:16 -0500

    feat:  components for Course Build

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,9 @@
+const ProgressBar = () => {
+    return (
+        <div>
+            <progress value="50" max="100"></progress>
+        </div>
+    );
+};
+
+export default ProgressBar;
---
+++ src/components/StepBar.tsx
@@ -0,0 +1,9 @@
+const StepBar = () => {
+    return (
+        <div>
+            <h1>StepBar</h1>
+        </div>
+    );
+};
+
+export default StepBar;
